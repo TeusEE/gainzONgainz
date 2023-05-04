70ea65202f74592f73cbd2519c695ffd6a168df8
taewoolee <xodn1311@knou.ac.kr>
2023-05-04 20:46:57 +0900

    [feat] Add DrawerComponents and fit the size

--- GainzOnGainz/screens/DietScreen.js
+++ GainzOnGainz/screens/DietScreen.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DrawerDate from './../components/DrawerDate'
 
 function DietScreen() {
   return (
     <View style={styles.block}>
+      <DrawerDate/>
       <Text>DietScreen</Text>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   block: {
-    flex:1
   },
 });
 
