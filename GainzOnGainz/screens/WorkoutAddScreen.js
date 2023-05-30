43794c4a193cb94e15243f9656cdf984bd287610
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2023-05-30 19:05:50 +0900

    [feat] create WorkoutAddScreen UI
    - 상단 저장 및 닫기 버튼 추가
    - 하단 세트 추가하기 버튼 추가

--- GainzOnGainz/screens/WorkoutAddScreen.js
+++ GainzOnGainz/screens/WorkoutAddScreen.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import {Text, StyleSheet, View, TextInput} from 'react-native';
+import {Text, StyleSheet, View, TextInput, TouchableOpacity} from 'react-native';
 import WorkoutAddListItem from '../components/WorkoutAddListItem';
 
-function WorkoutAddScreen() {
+function WorkoutAddScreen({navigation}) {
   return (
-    <View style={styles.block}>
+    <View style={styles.body}>
+      <View style={styles.listView}>
+        <TouchableOpacity onPress={() => navigation.pop()} >
+          <Text style={styles.close}>닫기</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.saveView}>
+            <Text style={styles.saveText}>저장</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.separator}/>
+      <View style={styles.block}>
+        <View style={styles.workoutGroupView}>
+          <View style={styles.workoutGroup}>
+            <Text style={styles.workoutGroupText}>상체</Text>
+          </View>
+          <View style={styles.workoutType}>
+            <Text style={styles.workoutTypeText}>가슴</Text>
+          </View>
+        </View>
         <TextInput 
             style={styles.workoutTitle}
            placeholder="운동명"/>
@@ -15,15 +35,26 @@ function WorkoutAddScreen() {
             <WorkoutAddListItem title="휴식" unit="초" hint="30"/>
         </View>
         <View style={styles.separator}/>
+        <TouchableOpacity>
+        <View style={styles.addButtonView}>
+          <Text style={styles.addButtonText}>세트 추가하기</Text>
+        </View>
+      </TouchableOpacity>
+      </View>
     </View>
   );
+
 }
 
 
+
 const styles = StyleSheet.create({
-  block: {
+  body:{
     flex:1,
     backgroundColor:"#FFF",
+  },
+  block: {
+    flex:1,
     paddingHorizontal:25
   },
   separator:{
@@ -39,10 +70,64 @@ const styles = StyleSheet.create({
     justifyContent:'space-between',
     marginVertical:18
   },
+  saveView:{
+    backgroundColor:"#000",
+    borderRadius:50,
+    paddingHorizontal:20,
+    paddingVertical:8,
+    marginHorizontal:25
+  },
+  saveText:{
+    color:"#FFF",
+    fontSize:16,
+    fontWeight:"600"
+  },
+  close:{
+    marginHorizontal:25,
+  },
   workoutTitle:{
     fontSize:24,
     fontWeight:"600",
     marginVertical:20
+  },
+  workoutGroupView:{
+    flexDirection: 'row',
+    marginVertical:20
+  },
+  workoutGroup:{
+    borderRadius:50,
+    backgroundColor:"#FF7373",
+    paddingVertical:4,
+    paddingHorizontal:12
+  },
+  workoutGroupText:{
+    color:"#FFF",
+    fontSize:15,
+    fontWeight:"600"
+  },
+  workoutType:{
+    borderRadius:50,
+    borderWidth: 1,
+    borderRadius: 36,
+    borderStyle: 'solid',
+    paddingVertical:4,
+    paddingHorizontal:12,
+    marginHorizontal:8
+  },
+  workoutTypeText:{
+    fontSize:15,
+    fontWeight:"600"
+  },
+  addButtonView:{
+    backgroundColor:"#F4F4F4",
+    borderRadius:16,
+    paddingVertical:24,
+    marginVertical:30,
+    alignItems:'center'
+  },
+  addButtonText:{
+    color:"#BDBDBD",
+    fontSize:15
   }
 });
 
