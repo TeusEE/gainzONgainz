fe0addc390c10aa19e346b16b89e5777f862a6b3
leesukkyeong <104298444+leesukkyeong@users.noreply.github.com>
2023-06-11 09:47:14 +0900

    add workoutListBody

---
+++ GainzOnGainz/components/WorkoutListBody.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, Modal, TouchableOpacity } from 'react-native';
+
+function WorkoutListItem({ type, name, weight, count }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedType, setSelectedType] = useState('');
+  const [selectedSubType, setSelectedSubType] = useState('');
+
+  const openModal = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  const handleTypeSelection = (type) => {
+    setSelectedType(type);
+    setSelectedSubType('');
+  };
+
+  const handleSubTypeSelection = (subType) => {
+    setSelectedSubType(subType);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={openModal}>
+        <Text style={styles.text}>운동 분위 선택</Text>
+      </TouchableOpacity>
+
+      <Modal visible={modalVisible} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalHeaderText}>운동 분위 선택</Text>
+
+          <TouchableOpacity
+            style={[styles.optionButton, selectedType === '상체' && styles.selectedOption]}
+            onPress={() => handleTypeSelection('상체')}
+          >
+            <Text style={styles.optionButtonText}>상체</Text>
+          </TouchableOpacity>
+
+          {selectedType === '상체' && (
+            <View>
+              <TouchableOpacity
+                style={[styles.subOptionButton, selectedSubType === '어깨' && styles.selectedSubOption]}
+                onPress={() => handleSubTypeSelection('어깨')}
+              >
+                <Text style={styles.subOptionButtonText}>어깨</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+
+          <TouchableOpacity onPress={closeModal}>
+            <Text style={styles.closeButtonText}>닫기</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 200,
+  },
+  modalHeaderText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  optionButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#eee',
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  selectedOption: {
+    backgroundColor: 'green',
+  },
+  optionButtonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  subOptionButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: '#ccc',
+    marginBottom: 5,
+    borderRadius: 3,
+    marginLeft: 5,
+  },
+  subOptionButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  closeButtonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'blue',
+    marginTop: 20,
+  },
+});
+
+export default Workout
